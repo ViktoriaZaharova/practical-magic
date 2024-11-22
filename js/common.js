f704106f4da3c885ce4663d8ef87c804148979cf
Viktoria Zaharova <zaharova181114@gmail.com>
2024-11-22 23:58:39 +0200

    1

--- js/common.js
+++ js/common.js
@@ -11,3 +11,59 @@ $('.go_to').click(function (e) {
 });
 
 
+// accordeon
+function accordeon() {
+	var panel = $('.panel_heading');
+
+	if (panel.hasClass('in')) {
+		$('.in').find('.block_hover').slideDown();
+	}
+
+	$('.panel_heading .block_title').on('click', function () {
+		$(this).parent().toggleClass('in').find('.block_hover').slideToggle();
+	});
+}
+
+accordeon();
+
+// модальные окна (несколько)
+$(function () {
+  let overlay = $('.overlay'),    
+      open_modal = $('.open_modal'),
+      close = $('.modal__close, .overlay'),
+      modal = $('.modal__div');
+
+  open_modal.on('click',function (event) {
+      event.preventDefault();
+
+      modal.css('display', 'none').animate({
+          opacity: 0,
+          top: '45%'
+      }, 200);
+
+      let div = $(this).attr('href');
+      overlay.fadeIn(400,
+          function () {
+              $(div)
+                  .css('display', 'flex')
+                  .animate({
+                      opacity: 1,
+                      top: '50%'
+                  }, 200);
+          });
+  });
+
+  close.on('click', function () {
+      modal
+          .animate({
+                  opacity: 0,
+                  top: '45%'
+              }, 200,
+              function () {
+                  $(this).css('display', 'none');
+                  overlay.fadeOut(400);
+              }
+          );
+  });
+});
+//end
